d910dde384c8ed5ee8be2c8eb7f548b13d0b4af0
Dunkaburk <jonte8000@hotmail.com>
2023-02-08 21:02:44 +0100

    new level init stuff

---
+++ Assets/scripts/levelScripts/BoardManager.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class BoardManager : MonoBehaviour
+{
+    [Seralizable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+
+        public Count(int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+        
+    }
+
+    //The size of the map as a grid. Currently 8x8
+    public int columns = 8;
+    public int rows = 8;
+
+    //Exits on the map. Between 1 and 4.
+    public GameObject[] exitTiles;
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    private void initializeList()
+    {
+        gridPositions.Clear();
+
+        for (int x = 1; x < columns - 1; x++)
+        {
+            for (int y = 1; y < rows - 1; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
+
+    private void boardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        for (int x = -1; x < columns + 1; x++)
+        {
+            for (int y = -1; y < rows + 1; y++)
+            {
+                GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                {
+                    toInstantiate = wallTiles[Random.Range(0, wallTiles.Length)];
+                }
+
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    public Vector3 randomPosition()
+    {
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector3 randomPosition = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex); //Removes the position from the list so it can't be used again.
+        return randomPosition;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
