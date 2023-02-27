687d938a85512a14de83ad169e455406b11672a3
Zaloz64 <Zoe@zalo.se>
2023-02-27 20:48:13 +0100

    stuffs

---
+++ Assets/scripts/ElevatorScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorScript : MonoBehaviour
+{
+
+    private GameObject player;
+    public float distance= 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < distance)
+        {
+            if (Input.GetKeyDown(KeyCode.R)) {
+                Debug.Log("reset level");
+            }
+        }
+    }
+}
