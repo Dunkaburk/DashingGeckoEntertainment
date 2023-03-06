d9cfd45548c1bee712fd890a6a4e4265a8f474f1
Zaloz64 <Zoe@zalo.se>
2023-03-06 20:34:05 +0100

    shop working

--- Assets/scripts/interactable/ShopScript.cs
+++ Assets/scripts/interactable/ShopScript.cs
@@ -8,6 +8,9 @@ public class ShopScript : MonoBehaviour
     private interactableObject ib;
     public float distance = 10;
     public GameObject panel;
+    public GameObject[] speach;
+    private bool interakting = false;
+    private int intercount;
 
     // Start is called before the first frame update
     void Start()
@@ -22,27 +25,64 @@ public class ShopScript : MonoBehaviour
     {
         if (ib.interacted)
         {
-            panel.SetActive(true);
+            interakting = true;
+            //panel.SetActive(true);
         }
+
+        if (interakting)
+        {
+            if(Input.GetKeyUp(KeyCode.E))
+            {
+                intercount++;
+            }
+
+            if (intercount <= 3 && intercount != 0)
+            {
+                Debug.Log(intercount);
+                if(intercount > 1)
+                    speach[intercount-2].SetActive(false);
+                speach[intercount-1].SetActive(true);
+            }
+            else if (intercount == 4)
+            {
+                speach[intercount - 2].SetActive(false);
+                panel.SetActive(true);
+            }
+        }
+
     }
 
     public void ClosePanel()
     {
         panel.SetActive(false);
+        interakting = false;
+        intercount = 0;
     }
 
-    public void AddTime()
+    public void AddTime(int amount)
     {
-
+        if (GameManager.coins >= amount)
+        {
+            GameManager.coins -=amount;
+            GameManager.startTime += 10;
+        }
     }
 
-    public void heal()
+    public void heal(int amount)
     {
-
+        if (GameManager.coins >= amount)
+        {
+            Debug.Log("gere");
+            GameManager.coins -= amount;
+            GameManager.health = 100;
+        }
     }
 
-    public void addSpeed()
+    public void buyPart(int amount)
     {
-
+        if (GameManager.coins >= amount)
+        {
+            GameManager.coins -= amount;
+        }
     }
 }
